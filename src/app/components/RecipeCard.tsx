17ab4e93c6cab1dc69227d5a7e47f732e662e598
Josh Oladunni <josholadunni@gmail.com>
2025-04-20 19:39:33 +0100

    Initial formatting of minutes into hours

--- src/app/components/RecipeCard.tsx
+++ src/app/components/RecipeCard.tsx
@@ -12,17 +12,38 @@ interface RecipeCard {
 function RecipeCard(props: RecipeCard) {
   const [isLiked, setisLiked] = useState<boolean>(false);
   const { title, photoUrl, cookTime } = props;
+
+  const formattedCookTime = () => {
+    if (cookTime) {
+      if (cookTime >= 60) {
+        let hours = null;
+        hours = Math.floor((cookTime / 60) * 10) / 10;
+        return `${hours} hours`;
+      } else {
+        return `${cookTime} mins`;
+      }
+    }
+  };
+
   return (
-    <div className="w-48 p-2 shadow-md">
-      <div className="h-20 flex flex-col justify-end">
-        <h2 className="text-lg leading-6 font-bold text-center">{title}</h2>
-      </div>
-      <div className="bg-white">
+    <div className="w-48 p-2">
+      <div className="w-fit">
         <div className="relative size-48 mt-4">
-          <Image src={photoUrl} alt={title} loading="lazy" fill={true} />
+          <Image
+            src={photoUrl}
+            alt={title}
+            loading="lazy"
+            fill={true}
+            className="object-cover"
+          />
         </div>
-        <div>{cookTime}</div>
-        <LikeButton isLiked={isLiked} setIsLiked={setisLiked} />
+        <div className="bg-white px-4 pb-2">
+          <div className="h-30 flex flex-col pt-3">
+            <h2 className="text-lg leading-6 font-bold text-center">{title}</h2>
+          </div>
+          <div>{formattedCookTime()}</div>
+          <LikeButton isLiked={isLiked} setIsLiked={setisLiked} />
+        </div>
       </div>
     </div>
   );
